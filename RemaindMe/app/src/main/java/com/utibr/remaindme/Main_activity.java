1ae840a897c6b11476b2e9574e9f157d57012870
Evgen True <okiboss1@gmail.com>
2017-07-05 22:19:28 +0300

    main_activity

---
+++ RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
@@ -0,0 +1,17 @@
+package com.utibr.remaindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Evgen True on 05.07.2017.
+ */
+
+public class Main_activity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
