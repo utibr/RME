2ea8879ef941ad04c1ef65bafe4bfb1beb63c3b0
Evgen True <okiboss1@gmail.com>
2017-07-05 23:36:11 +0300

    toolbar

--- RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
+++ RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
@@ -3,15 +3,34 @@ package com.utibr.remaindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Evgen True on 05.07.2017.
  */
 
 public class Main_activity extends Activity{
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
