f23a45fa3d7561f35ab12ed70c72cd0faecdfa09
Evgen True <okiboss1@gmail.com>
2017-07-06 23:32:37 +0300

    rev 2.1

--- RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
+++ RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
@@ -1,23 +1,26 @@
 package com.utibr.remaindme;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
 
 /**
  * Created by Evgen True on 05.07.2017.
  */
 
-public class Main_activity extends Activity{
+public class Main_activity extends AppCompatActivity{
     private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         initToolbar();
     }
 
