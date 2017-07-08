f5228c68f3ba785de659dd578f2d6cd25d123aa9
Evgen True <okiboss1@gmail.com>
2017-07-08 15:25:51 +0300

    rev 3.1

--- RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
+++ RemaindMe/app/src/main/java/com/utibr/remaindme/Main_activity.java
@@ -2,27 +2,31 @@ package com.utibr.remaindme;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.MenuItem;
 
 
-/**
- * Created by Evgen True on 05.07.2017.
- */
 
 public class Main_activity extends AppCompatActivity{
+    private  static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
+
+
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -35,5 +39,11 @@ public class Main_activity extends AppCompatActivity{
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_Layout);
+
     }
 }
